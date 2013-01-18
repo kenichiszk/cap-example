267fd872a4e259c74b4acea221fe7e3141375d60
Kenichi suzuki <kenichis@nttpc.co.jp>
2013-01-18 14:44:17 +0900

    ...

--- config/deploy/staging.rb
+++
@@ -1,9 +0,0 @@
-role :app, "192.168.1.10", :user => rails_user
-role :proxy, "192.168.1.10", :no_release => true
-role :web, "192.168.1.10", :no_release => true
-role :db, "192.168.1.10", {:primary => true, :no_release => true}
-
-set :ntp_server, "ntp.jst.mfeed.ad.jp"
-set :unicorn_port, 3100
-set :unicorn_pid_dir, "tmp/pids"
-set :unicorn_log_dir, "log"
